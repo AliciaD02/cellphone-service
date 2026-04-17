38f51e0de60e21b395a02076fc1f454a62d80af5
AliciaD02 <diazalicia4542@gmail.com>
2026-04-16 23:49:13 -0500

    commit :0

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,75 @@
 package com.pluralsight;
 
 public class CellPhone {
+
+    //Data numbers of the class'
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    // the input
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+     public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "CellPhone{" +
+                "serialNumber=" + serialNumber +
+                ", model='" + model + '\'' +
+                ", carrier='" + carrier + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
 }
+
+
+
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,53 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    // scanner to get info from user
+    public static Scanner theScanner= new Scanner(System.in);
+    public static void main(String[] args) {
+
+        // create the phone
+        CellPhone thePhone = new CellPhone();
+
+        // start gathering information for the phone
+        System.out.println("what is the serial number? ");
+
+        //start the serial from the user
+        int serialNumber = theScanner.nextInt();
+
+        // add the serialNumber to the actual phone
+        // this is using the phone set method
+        thePhone.setSerialNumber(serialNumber);
+
+        //eat the newline since switching from numbers to text
+        theScanner.nextLine();
+
+        System.out.println("What model is this phone? ");
+        String model = theScanner.nextLine();
+
+        // after this you have to add the model to the phone
+
+        thePhone.setModel(model);
+
+        System.out.println("Who is the carrier?");
+        String carrier = theScanner.nextLine();
+
+        thePhone.setCarrier(carrier);
+
+        System.out.println("What is the phone number? ");
+        String phoneNumber = theScanner.nextLine();
+
+        thePhone.setPhoneNumber(phoneNumber);
+
+        System.out.println("Who is the owner of this phone? ");
+        String owner = theScanner.nextLine();
+        thePhone.setOwner(owner);
+
+        System.out.println(thePhone.toString());
+
+    }
+
+
 }
+
